25aadee5bfd10e0ba196ab75ddf3a40efa3456f9
Dan_dev <danillo.schwager06@gmail.com>
2025-09-15 13:38:41 -0300

    Initial commit

---
+++ src/test/java/demo/java/javaproject/JavaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.java.javaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
